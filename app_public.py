9b52359db11ff78f3a5d186abcee2955478d5e9f
tomasgrno1-stack <tomasgrno1@outlook.com>
2026-09-13 19:05:44 +0200

    Update app_public.py

--- app_public.py
+++ app_public.py
@@ -15,6 +15,9 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded"
 )
+# 1. Automatické obnovovanie aplikácie (každých 60 sekúnd = 60000 ms)
+# Toto posiela neviditeľný "ping", aby Streamlit cloud aplikáciu neuspal pri nečinnosti
+st_autorefresh(interval=60000, limit=None, key="polaris_keep_alive")
 
 # 2. Rýchla detekcia jazyka s krátkym timeoutom (max 1 sekunda)
 @st.cache_data(ttl=86400)
